dddb505e16849249bb5b597ae9d27dcc90e5059a
Marat Field <mrpolyansky@yandex.ru>
2022-11-07 21:22:03 +0300

    Firts code

---
+++ src/main/java/ru/netology/BillboardMng/services/BillboardItem.java
@@ -0,0 +1,10 @@
+package ru.netology.BillboardMng.services;
+
+public class BillboardItem {
+    String productName;
+
+    public BillboardItem(String productName) {
+        this.productName = productName;
+    }
+
+}
